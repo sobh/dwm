c90c2d60676f839bc84d44c3e6537a4f32b88dd7
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-04-18 13:36:26 +0200

    config.h | Changed the key bindings to my liking.

--- config.h
+++ config.h
@@ -1,5 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/X.h>
+#include <X11/keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -45,7 +48,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -62,29 +65,42 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+
+	/* ------------------------ General ------------------------ */
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
+	{ MODKEY|ControlMask,           XK_q,      quit,           {0} },
+
+	/* ------------------------ Layout ------------------------ */
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },    /* Tiling  Layout */
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },    /* Foating Layout */
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },    /* Monocol Layout */
+	{ MODKEY,                       XK_space,  setlayout,      {0} },                   /* Toggle Between the current and previous layout */
+	{ MODKEY|ControlMask,           XK_f,      togglefloating, {0} },                   /* Toggle the focused window between the tiled and floating state. */
+
+	/* ------------------------ Focus ------------------------ */
+	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_h,      focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_l,      focusmon,       {.i = +1 } },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+
+	/* ------------------------ Resize ------------------------ */
+	{ MODKEY|ControlMask,           XK_l,      setmfact,       {.f = +0.05} },          /* Increase the Master Area size */
+	{ MODKEY|ControlMask,           XK_h,      setmfact,       {.f = -0.05} },          /* Decrease the Master Area size */
+
+	/* ------------------------ Shifting ------------------------ */
+	{ MODKEY|ShiftMask,             XK_m,      zoom,           {0} },                   /* Cycle the focused window to/from the Master Area */
+	{ MODKEY|ShiftMask,             XK_h,      tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_l,      tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_j,      incnmaster,     {.i = +1 } },            /* Increase the number of windows in the Master Area size */
+	{ MODKEY|ShiftMask,             XK_k,      incnmaster,     {.i = -1 } },            /* Decrease the number of windows in the Master Area size */
+
+	/* ------------------------ Tagging ------------------------ */
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -94,7 +110,6 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
 /* button definitions */
