5b86aa11c5184c8bd7e0559216b39e776c1490be
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-04-23 18:53:34 +0200

    config.h | Use the 'terminal' script to start the default terminal.

--- config.h
+++ config.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray, "-sb", col_accent, "-sf", col_white, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "terminal", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
